b63d2096a50d3dff6be1609e08f6384b342a88e4
anonyDev <phuochh@nortstudio.vn>
2023-11-09 00:04:48 +0700

    handle  service

--- app/middleware/auth.middleware.js
+++ app/middleware/auth.middleware.js
@@ -1,19 +1,18 @@
 const jwt = require('jsonwebtoken');
+const {responseData} = require("../constant/responseData");
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // Kiểm tra xem yêu cầu có Header 'Authorization' và có dạng 'Bearer <token>' không.
 
     if (!token) {
-        return res.status(401).json({ message: 'Token không hợp lệ.' });
+        return res.status(401).json(responseData(false, {}, 'Token không hợp lệ.'));
     }
 
     jwt.verify(token, 'your_secret_key', (err, user) => {
         if (err) {
-            return res.status(403).json({ message: 'Xác thực thất bại.' });
+            return res.status(403).json(responseData(false, {}, 'Xác thực thất bại.'));
         }
-
-        // Gắn thông tin người dùng vào req.user
         req.user = user;
         next();
     });

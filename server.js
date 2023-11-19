c562165c4319dc8e264a41c5670c30f36e65a04e
anonyDev <phuochh@nortstudio.vn>
2023-11-19 17:33:44 +0700

    f

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ require("./app/routes/service.routes")(app);
 require("./app/routes/users.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });

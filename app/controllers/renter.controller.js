98ffd36412b82ea6b3190e67ed7473a7104af1c6
anonyDev <phuochh@nortstudio.vn>
2023-12-09 22:30:58 +0700

    f

--- app/controllers/renter.controller.js
+++ app/controllers/renter.controller.js
@@ -2,85 +2,102 @@ const { responseData } = require("../constant/responseData");
 const db = require("../models");
 const Renter = db.renters;
 
-
 exports.getAll = async (req, res, next) => {
   try {
-    const allRenters = await Renter.find({})
-    res.json(responseData(true, {
-      renters: req.user.role == "admin" ? allRenters : allRenters.map(item => {
-        const { sold, ...info } = item._doc
-        return info
-      })
-    }, 'lấy thông tin thiết bị thành công'));
+    const allRenters = await Renter.find({}).lean();
+    res.json(
+      responseData(
+        true,
+        {
+          renters: allRenters.map((item) => {
+            const { sold, ...info } = item;
+            return { ...info, id: info._id };
+          }),
+        },
+        "lấy thông tin thiết bị thành công"
+      )
+    );
   } catch (e) {
-    return res.json(responseData(false, {}, "Lỗi máy chủ"))
+    return res.json(responseData(false, {}, "Lỗi máy chủ"));
   }
 };
 
 exports.create = async (req, res, next) => {
   try {
-    const { name, unit, price, note, quantity } = req.body
+    const { name, unit, price, note, quantity } = req.body;
     if (!name || !unit || !price || !quantity) {
-      return res.json(responseData(false, {}, "các trường chưa hợp lệ"))
+      return res.json(responseData(false, {}, "các trường chưa hợp lệ"));
     }
-    const existedRenter = await Renter.findOne({ name })
+    const existedRenter = await Renter.findOne({ name });
     if (existedRenter) {
-      return res.json(responseData(false, {}, "Thiết bị đã tồn tại"))
+      return res.json(responseData(false, {}, "Thiết bị đã tồn tại"));
     }
     const newRenter = new Renter({
-      ...req.body
-    })
-    await newRenter.save()
-    res.json(responseData(true, { renter: newRenter }, 'Thêm thiết bị thành công'));
+      ...req.body,
+    });
+    await newRenter.save();
+    res.json(
+      responseData(true, { renter: newRenter }, "Thêm thiết bị thành công")
+    );
   } catch (e) {
-    return res.json(responseData(false, {}, "Lỗi máy chủ"))
+    return res.json(responseData(false, {}, "Lỗi máy chủ"));
   }
-}
-
+};
 
 exports.update = async (req, res, next) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     if (!id) {
-      return res.json(responseData(false, {}, "Id không hợp lệ"))
+      return res.json(responseData(false, {}, "Id không hợp lệ"));
     }
-    const udpatedRenter = await Renter.findOneAndUpdate({ _id: id }, { ...req.body }, { new: true })
-    res.json(responseData(true, { renter: udpatedRenter }, 'Cập nhật thông tin thiết bị thành công'));
+    const udpatedRenter = await Renter.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true }
+    );
+    res.json(
+      responseData(
+        true,
+        { renter: udpatedRenter },
+        "Cập nhật thông tin thiết bị thành công"
+      )
+    );
   } catch (e) {
     console.log({ e });
-    return res.json(responseData(false, {}, "Lỗi máy chủ"))
+    return res.json(responseData(false, {}, "Lỗi máy chủ"));
   }
-}
+};
 
 exports.delete = async (req, res) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     if (!id) {
-      return res.json(responseData(false, {}, "Id không hợp lệ"))
+      return res.json(responseData(false, {}, "Id không hợp lệ"));
     }
-    const deleteRes = await Renter.deleteOne({ _id: id })
+    const deleteRes = await Renter.deleteOne({ _id: id });
     if (!deleteRes) {
-      return res.json(responseData(false, {}, "Lỗi máy chủ"))
+      return res.json(responseData(false, {}, "Lỗi máy chủ"));
     }
-    return res.json(responseData(true, {}, "xóa thiết bị thành công"))
+    return res.json(responseData(true, {}, "xóa thiết bị thành công"));
   } catch (e) {
-    return res.json(responseData(false, {}, "Lỗi máy chủ"))
+    return res.json(responseData(false, {}, "Lỗi máy chủ"));
   }
-}
-
+};
 
 exports.findById = async (req, res) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     if (!id) {
-      return res.json(responseData(false, {}, "Id không hợp lệ"))
+      return res.json(responseData(false, {}, "Id không hợp lệ"));
     }
-    const renter = await Renter.findById(id)
+    const renter = await Renter.findById(id);
     if (!renter) {
-      return res.json(responseData(false, {}, "Thiết bị không tồn tại"))
+      return res.json(responseData(false, {}, "Thiết bị không tồn tại"));
     }
-    return res.json(responseData(true, { renter }, "lấy thông tin thiết bị thành công"))
+    return res.json(
+      responseData(true, { renter }, "lấy thông tin thiết bị thành công")
+    );
   } catch (e) {
-    return res.json(responseData(false, {}, "Lỗi máy chủ"))
+    return res.json(responseData(false, {}, "Lỗi máy chủ"));
   }
-}
+};
